308774238e861503707297f6ca98955be0354aff
Nick Garbalau <garbalaunick@gmail.com>
2023-04-03 15:34:13 +0300

    Moved textures in needed folder

--- src/components/Earth/index.tsx
+++ src/components/Earth/index.tsx
@@ -1,10 +1,11 @@
 export const Earth = () => {
   // Render mesh
-
   return (
     <>
+      <ambientLight intensity={0.5} />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial color={'red'} />
       </mesh>
     </>
   );
